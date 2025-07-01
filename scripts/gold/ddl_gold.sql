535dee472193f98b2e930d3d74d0c127da85ac2e
marco-kraemer <marco.k.santos@hotmail.com>
2025-07-01 09:06:25 -0300

    Fix Errors

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -82,15 +82,15 @@ GO
 -- Purpose: Fact table containing sales transactions, linking customers and products
 CREATE VIEW gold.fact_sales AS
 SELECT
-	sd.sls_ord_num as order_number,
-	pr.product_key,  -- FK to dim_products
-	cu.customer_key, -- FK to dim_customers
-	sd.sls_cust_id as order_date,
-	sd.sls_order_dt as shipping_date,
-	sd.sls_ship_dt as due_date,
-	sd.sls_due_dt as sales_amount,
-	sd.sls_quantity as quantity,
-	sd.sls_price price
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key = pr.product_number
